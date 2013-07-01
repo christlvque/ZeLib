a75578bc83973740e5579eb6cccc225f40c99698
liorzoue <etienne.liorzou+github@gmail.com>
2013-07-01 15:56:59 +0200

    Create google-chart-api.js

---
+++ files/google-chart-api.js
@@ -0,0 +1,31 @@
+  /* New ! begin supporting Google Chart API */
+	/* Very beginning */
+	_ZeExtend({ chart: function (type, data, opts) {
+			var out = false;
+			
+			/* Implement Google Chart API */
+			
+			if (!window.google) { throw 'Google API not detected !'; return false; }
+			else { _ZeLib.log.basic('Google API OK'); }
+			
+			switch(type) {
+				case 'pareto':
+				case 'barchart':
+				{
+					_ZeLib.log.basic('Chart type selected : ' + type);
+					out = true;
+					break;
+				}
+				default:
+				{
+					_ZeLib.log.warn('Unknown chart type : ' + type);
+					break;
+				}
+			}
+			
+			return out;
+			
+		}	
+	});
+	
+	
